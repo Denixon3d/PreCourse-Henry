f9093c7a22da2bfcc8bd13d9dbbd1748eb1a98f3
Denixon3d <99200334+Denixon3d@users.noreply.github.com>
2023-03-11 18:28:20 -0500

    variables

---
+++ variables.js
@@ -0,0 +1,30 @@
+//String
+
+let bootcamp = "Henry";
+
+//Numeros
+
+let numerosEnteros = 20;
+let numerosNegativos = -20;
+let numerosDecimal = 20.5;
+
+//Booleanos
+
+let estoyEntendiendo = true;
+
+let estoyAburrido = false;
+
+//Undefined
+
+let cajon1;
+
+console.log(cajon1);
+
+//Null
+
+let cajon2 = null;
+
+console.log(cajon2);
+
+//NaN
+
