1360c81f79c07d66d93b4744c42bebe04d95c770
Denixon3d <99200334+Denixon3d@users.noreply.github.com>
2023-03-11 18:44:41 -0500

    operadores

--- operadores.js
+++ operadores.js
@@ -1,16 +1,18 @@
+//Suma
 console.log(3 + 5);
 
+//Resta
 console.log(14 - 7);
 
+//Multiplicacion
 console.log(4 * 6);
 
+//Division
 console.log(30 / 9);
 
+//Resto
 console.log(95 % 4);
 
+//Precedencia
 console.log((3 + 5) * 2 - 8);
 
-let nombre = "camilo";
-let saludo = "Hola! ";
-
-console.log(saludo + nombre);
