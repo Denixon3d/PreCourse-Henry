a8cd7ddc9d786bec96056be80360ad467e02ebb1
Denixon3d <99200334+Denixon3d@users.noreply.github.com>
2023-03-11 19:35:17 -0500

    funciones

---
+++ funciones.js
@@ -0,0 +1,16 @@
+//Función: sumaTres
+let sumaTres = (x) => {
+    return x + 3;
+}
+
+//Función: cuidadoConElConsoleLog
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+}
+
+//Precedencia de valores
+let a = 1;
+let b = 2;
+let c = a = b;
+
+console.log (c); 
