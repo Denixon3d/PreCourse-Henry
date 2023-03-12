a7b6cadb7a45333acce8329e4b52e3f7cb113aab
Denixon3d <99200334+Denixon3d@users.noreply.github.com>
2023-03-12 14:33:28 -0500

    bucles

---
+++ bucles.js
@@ -0,0 +1,15 @@
+//FOR
+let suma = 0;
+
+for (let i = 0; i < 5; i++) {
+    suma = suma + i;
+    console.log('Variable de iteración: ', i);
+};
+console.log('Variable suma: ', suma);
+
+//WHILE
+
+while(suma < 3) {
+    suma = suma + 1;
+    console.log(suma);
+};
