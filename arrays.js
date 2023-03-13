11de770ae122dfbfee00a747fc5ee08a66333764
Denixon3d <99200334+Denixon3d@users.noreply.github.com>
2023-03-13 18:08:58 -0500

    arrays

---
+++ arrays.js
@@ -0,0 +1,13 @@
+// Lista de compras
+
+listaDeCompras[3] = 'Tomates';
+listaDeCompras[1] = 'Lechuga';
+
+// Ver lista de compras
+
+let elementoDelArray = listaDeCompras[1];
+console.log(elementoDelArray);
+
+// Lenght
+let nombres = ['Matias', 'Maria', 'Diego', 'Rosa'].length;
+console.log(nombres);
